2f827f5253fd56bf4a9b261f1fab4f4a72cfac26
Karl Lee <thatkoreanguy1@gmail.com>
2013-04-15 20:47:41 -0400

    match 3 score logic implemented

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -16,12 +16,39 @@
     if ([otherCards count] == 1) {
         PlayingCard *otherCard = [otherCards lastObject];
         if ([otherCard.suit isEqualToString:self.suit]) {
-            score = 1;
+            score = 2;
         } else if (otherCard.rank == self.rank) {
-            score = 4;
+            score = 5;
         }
     } else if ([otherCards count] == 2){
+        PlayingCard *card1 = otherCards[0];
+        PlayingCard *card2 = otherCards[1];
         
+        // account for matching all 3
+        // check suit first
+        if ([self.suit isEqualToString:card1.suit] && [self.suit isEqualToString:card2.suit]) {
+            // if suits match, you win lots of points
+            score = 70;
+            
+            // check to see if ranks match (no need to check further)
+        } else if ((self.rank == card1.rank) && (self.rank == card2.rank)) {
+            score = 2200;
+
+            // account for matching 2
+        } else if ([self.suit isEqualToString:card1.suit] || [self.suit isEqualToString:card2.suit]) {
+            // check suits first
+            // check if ranks match as well
+            if ((self.rank == card1.rank) || (self.rank == card2.rank)) {
+                // if so, then awesome for you!
+                score = 4000;
+            } else {
+                // if not, you still win points
+                score = 20;
+            }
+        } else if ((self.rank == card1.rank) || (self.rank == card2.rank)) {
+            // if only ranks match, score is given as well
+            score = 200;
+        }
     }
     
     return score;
